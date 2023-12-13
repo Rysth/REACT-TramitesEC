511cf256b893da53f80666ae8296825f6136febb
Rysth <johnpalacios.t@gmail.com>
2023-12-13 10:58:46 -0500

    Added react-paginate.

--- src/pages/Customer/Customer.jsx
+++ src/pages/Customer/Customer.jsx
@@ -3,13 +3,15 @@ import { customerActions } from '../../redux/slices/CustomerSlice'
 import NavBar from '../../components/NavBar/NavBar'
 import TableSearch from '../../components/Table/TableSearch'
 import CustomerTable from './components/CustomerTable'
-import CustomerStats from './components/CustomerStats'
+/* import CustomerStats from './components/CustomerStats' */
 
 function Customer() {
   return (
     <section className="px-4 sm:px-8 sm:pb-4 sm:ml-64" id="customerSection">
-      <NavBar />
-      <CustomerStats />
+      <header>
+        <NavBar />
+        {/* <CustomerStats /> */}
+      </header>
       <main>
         <TableSearch
           title="Listado de Clientes"
--- src/pages/Customer/components/CustomerTable.jsx
+++ src/pages/Customer/components/CustomerTable.jsx
@@ -1,20 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import InfiniteScroll from 'react-infinite-scroll-component'
+import ReactPaginate from 'react-paginate'
 import Loading from '../../../components/Loading/Loading'
 import { getClientes, destroyCliente } from '../../../redux/slices/CustomerSlice'
 
 function CustomerTable() {
   const dispatch = useDispatch()
-  const [records, setRecords] = useState(12)
   const { customersFilter, loading } = useSelector((store) => store.customer)
   const activeToken = useSelector((store) => store.authentication.activeToken)
   const [customerID, setCustomerID] = useState(null)
   const [showModal, setShowModal] = useState(false)
-
-  const increaseDisplayCount = () => {
-    setRecords(records + 12)
-  }
+  const [currentPage, setCurrentPage] = useState(0)
+  const itemsPerPage = 8
 
   const onDeleteCustomer = (customerID) => {
     dispatch(destroyCliente({ activeToken, customerID }))
@@ -25,12 +22,21 @@ function CustomerTable() {
     dispatch(getClientes(activeToken))
   }, [dispatch, activeToken])
 
+  const pageCount = Math.ceil(customersFilter.length / itemsPerPage)
+
+  const handlePageClick = ({ selected }) => {
+    setCurrentPage(selected)
+  }
+
+  const offset = currentPage * itemsPerPage
+  const currentItems = customersFilter.slice(offset, offset + itemsPerPage)
+
   if (loading) {
     return <Loading />
   }
 
   return (
-    <div className="relative mb-10" id="scrollableDiv">
+    <div className="mb-10" id="scrollableDiv flex-1">
       {showModal && (
         <div className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%)] max-h-full  grid place-items-center bg-black/50">
           <div className="relative w-full max-w-md max-h-full p-4">
@@ -58,91 +64,94 @@ function CustomerTable() {
           </div>
         </div>
       )}
-      <InfiniteScroll
-        dataLength={records}
-        next={increaseDisplayCount}
-        hasMore={records < customersFilter.length}
-        loader={<Loading />}
-        scrollableTarget="scrollableDiv"
-        className="overflow-auto max-h-96"
-      >
-        <table className="relative w-full text-sm text-left text-gray-500">
-          <thead className="sticky top-0 w-full text-xs text-gray-700 uppercase bg-gray-100">
-            <tr>
-              <th scope="col" className="w-12 px-6 py-3">
-                #
+      <table className="w-full overflow-auto text-sm text-left text-gray-500 max-h-40">
+        <thead className="w-full text-xs text-gray-700 uppercase bg-gray-100">
+          <tr>
+            <th scope="col" className="w-12 px-6 py-3">
+              #
+            </th>
+            <th scope="col" className="w-32 px-6 py-3">
+              Cédula
+            </th>
+            <th scope="col" className="w-40 px-6 py-3">
+              Nombre Completo
+            </th>
+            <th scope="col" className="w-32 px-6 py-3">
+              Celular
+            </th>
+            <th scope="col" className="w-40 px-6 py-3">
+              Email
+            </th>
+            <th scope="col" className="w-16 px-6 py-3">
+              Estado
+            </th>
+            <th scope="col" className="w-32 px-6 py-3">
+              Acciones
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentItems.map((customer, index) => (
+            <tr className="bg-white border-b hover:bg-gray-50" key={customer.id}>
+              <th scope="row" className="px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {customer.id}
               </th>
-              <th scope="col" className="w-32 px-6 py-3">
-                Cédula
-              </th>
-              <th scope="col" className="w-40 px-6 py-3">
-                Nombre Completo
-              </th>
-              <th scope="col" className="w-32 px-6 py-3">
-                Celular
-              </th>
-              <th scope="col" className="w-40 px-6 py-3">
-                Email
-              </th>
-              <th scope="col" className="w-16 px-6 py-3">
-                Estado
-              </th>
-              <th scope="col" className="w-32 px-6 py-3">
-                Acciones
-              </th>
+              <td className="px-6 py-2 truncate">{customer.cedula}</td>
+              <td className="px-6 py-2 truncate">{`${customer.nombres} ${customer.apellidos}`}</td>
+              <td className="px-6 py-2 truncate">{customer.celular}</td>
+              <td className="px-6 py-2 truncate">
+                <a
+                  href="mailto:johnpalacios.t@gmail.com"
+                  className="inline-block text-blue-500 underline md:hover:scale-105 md:transition"
+                >
+                  {customer.email}
+                </a>
+              </td>
+              <td className="px-6 py-2">
+                {customer.active ? (
+                  <span className="bg-green-100 text-green-800 text-sm font-medium me-2 px-2.5 py-1 rounded-full">
+                    Activo
+                  </span>
+                ) : (
+                  <span className="bg-indigo-100 text-indigo-800 text-sm font-medium me-2 px-2.5 py-1 rounded-full">
+                    Inactivo
+                  </span>
+                )}
+              </td>
+              <td className="flex items-center gap-2 px-6 py-4">
+                <button
+                  type="button"
+                  className="px-3 py-1 text-sm font-medium text-center text-white bg-blue-700 rounded-md hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 md:active:scale-95"
+                >
+                  Editar
+                </button>
+                <button
+                  type="button"
+                  className="px-3 py-1 text-sm font-medium text-center text-white bg-red-700 rounded-md hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 md:active:scale-95"
+                  onClick={() => {
+                    setShowModal(true)
+                    setCustomerID(customer.id)
+                  }}
+                >
+                  Eliminar
+                </button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {customersFilter.slice(0, records).map((customer, index) => (
-              <tr className="bg-white border-b hover:bg-gray-50" key={customer.id}>
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                  {index + 1}
-                </th>
-                <td className="px-6 py-4 truncate">{customer.cedula}</td>
-                <td className="px-6 py-4 truncate">{`${customer.nombres} ${customer.apellidos}`}</td>
-                <td className="px-6 py-4 truncate">{customer.celular}</td>
-                <td className="px-6 py-4 truncate">
-                  <a
-                    href="mailto:johnpalacios.t@gmail.com"
-                    className="inline-block text-blue-500 underline md:hover:scale-105 md:transition"
-                  >
-                    {customer.email}
-                  </a>
-                </td>
-                <td className="px-6 py-4">
-                  {customer.active ? (
-                    <span className="bg-green-100 text-green-800 text-sm font-medium me-2 px-2.5 py-1 rounded-full">
-                      Activo
-                    </span>
-                  ) : (
-                    <span className="bg-indigo-100 text-indigo-800 text-sm font-medium me-2 px-2.5 py-1 rounded-full">
-                      Inactivo
-                    </span>
-                  )}
-                </td>
-                <td className="flex items-center gap-2 px-6 py-4">
-                  <button
-                    type="button"
-                    className="px-3 py-1 text-sm font-medium text-center text-white bg-blue-700 rounded-md hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 md:active:scale-95"
-                  >
-                    Editar
-                  </button>
-                  <button
-                    type="button"
-                    className="px-3 py-1 text-sm font-medium text-center text-white bg-red-700 rounded-md hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 md:active:scale-95"
-                    onClick={() => {
-                      setShowModal(true)
-                      setCustomerID(customer.id)
-                    }}
-                  >
-                    Eliminar
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </InfiniteScroll>
+          ))}
+        </tbody>
+      </table>
+      <ReactPaginate
+        previousLabel={'<<'}
+        nextLabel={'>>'}
+        breakLabel={'...'}
+        pageCount={pageCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageClick}
+        containerClassName={'flex gap-1.5 mt-3 justify-end mt-5'}
+        activeClassName={'md:hover:scale-110 rounded-full bg-purple text-white'}
+        pageLinkClassName="p-2 text-sm py-1 md:hover:scale-110 block md:hover:shadow rounded-full md:hover:transition"
+      />
     </div>
   )
 }
