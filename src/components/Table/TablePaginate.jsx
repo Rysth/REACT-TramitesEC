63d865f463a407a55c68a1a5389acd50fb2efc12
Rysth <johnpalacios.t@gmail.com>
2023-12-15 19:57:04 -0500

    Create TablePaginate and usePagination hook.

---
+++ src/components/Table/TablePaginate.jsx
@@ -0,0 +1,24 @@
+import ReactPaginate from 'react-paginate'
+
+function TablePaginate({ currentPage, pageCount, handlePageChange }) {
+  return (
+    <footer>
+      <ReactPaginate
+        previousLabel={'<<'}
+        forcePage={currentPage}
+        nextLabel={'>>'}
+        breakLabel={'...'}
+        pageCount={pageCount}
+        marginPagesDisplayed={1}
+        pageRangeDisplayed={2}
+        onPageChange={handlePageChange}
+        containerClassName={'flex items-center gap-1 justify-center sm:justify-end mx-auto text-sm p-4'}
+        subContainerClassName={'mx-2'}
+        activeClassName={'active'}
+        pageLinkClassName="p-2 outline-1 block px-3"
+      />
+    </footer>
+  )
+}
+
+export default TablePaginate
--- src/pages/CustomerPage/CustomerPage.jsx
+++ src/pages/CustomerPage/CustomerPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getClientes } from '../../redux/slices/CustomerSlice'
 import SectionLayout from '../../layouts/SectionLayout/SectionLayout'
@@ -6,38 +6,20 @@ import HeaderLayout from '../../layouts/HeaderLayout'
 import MainLayout from '../../layouts/MainLayout'
 import TableLayout from '../../layouts/TableLayout'
 import TableHeader from '../../components/Table/TableHeader'
+import TablePaginate from '../../components/Table/TablePaginate'
 import CustomerTable from './components/CustomerTable'
 import { customerActions } from '../../redux/slices/CustomerSlice'
-import ReactPaginate from 'react-paginate'
+import usePagination from '../../hooks/usePagination'
 
 function CustomerPage() {
   const dispatch = useDispatch()
   const { activeToken } = useSelector((store) => store.authentication)
   const { customersArray } = useSelector((store) => store.customer)
-
-  const [currentPage, setCurrentPage] = useState(0)
-  const itemsPerPage = 30 // Set the number of items per page
-
-  const pageCount = Math.ceil(customersArray.length / itemsPerPage)
-
-  const handlePageChange = ({ selected }) => {
-    setCurrentPage(selected)
-  }
-
-  const startIndex = currentPage * itemsPerPage
-  const endIndex = startIndex + itemsPerPage
-
-  const currentItems = customersArray.slice(startIndex, endIndex)
-
-  const restartCurrentPage = () => setCurrentPage(0)
+  const { currentPage, pageCount, handlePageChange, currentItems, restartCurrentPage } = usePagination(customersArray)
 
   useEffect(() => {
     dispatch(getClientes(activeToken))
   }, [dispatch, activeToken])
-
-  useEffect(() => {
-    console.log(currentPage)
-  }, [currentItems])
 
   return (
     <SectionLayout>
@@ -51,22 +33,12 @@ function CustomerPage() {
         <TableLayout>
           <CustomerTable currentItems={currentItems} />
         </TableLayout>
-        <footer>
-          <ReactPaginate
-            previousLabel={'<<'}
-            forcePage={currentPage}
-            nextLabel={'>>'}
-            breakLabel={'...'}
-            pageCount={pageCount}
-            marginPagesDisplayed={1}
-            pageRangeDisplayed={2}
-            onPageChange={handlePageChange}
-            containerClassName={'flex items-center gap-1 justify-center sm:justify-end mx-auto text-sm p-4'}
-            subContainerClassName={'mx-2'}
-            activeClassName={'active'}
-            pageLinkClassName="p-2 outline-1 block px-3"
-          />
-        </footer>
+        <TablePaginate
+          currentPage={currentPage}
+          pageCount={pageCount}
+          handlePageChange={handlePageChange}
+          customArray={customersArray}
+        />
       </MainLayout>
     </SectionLayout>
   )
