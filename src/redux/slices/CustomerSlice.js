9d93ed8e66b5e07ade8ae5ff387ae08dfbd7ab8b
Rysth <johnpalacios.t@gmail.com>
2023-12-16 11:40:07 -0500

    Improve CustomerSlice by creating common methods.

--- src/redux/slices/CustomerSlice.js
+++ src/redux/slices/CustomerSlice.js
@@ -12,109 +12,117 @@ const initialState = {
   loading: true,
 }
 
-// GET Clientes#index
-export const getClientes = createAsyncThunk('customer/getClientes', async (activeToken) => {
-  try {
-    const response = await axios.get(`${API_URL}/api/v1/clientes`, {
-      headers: {
-        Authorization: atob(activeToken),
-      },
-      withCredentials: true,
-    })
+const handleRequestError = (error) => {
+  if (error.response.status === 401) {
+    toast.error('¡Sesión Caducada! Cerrando Sesion...', { autoClose: 2000, theme: 'colored' })
 
-    return response.data
-  } catch (error) {
-    if (error.response.status === 401) {
-      toast.error('¡Sesión Caducada! Cerrando Sesion...', { autoClose: 2000, theme: 'colored' })
-
-      setTimeout(() => {
-        sessionStorage.removeItem('active')
-        sessionStorage.removeItem('activeUser')
-        sessionStorage.removeItem('activeToken')
-
-        window.location.href = '/session'
-      }, 3000)
-    }
-
-    if (error.response.status === 500) {
-      toast.error('¡Problema en el Servidor!')
-    }
-
-    throw new Error(error)
+    setTimeout(() => {
+      sessionStorage.removeItem('active')
+      sessionStorage.removeItem('activeUser')
+      sessionStorage.removeItem('activeToken')
+      window.location.href = '/session'
+    }, 3000)
   }
+
+  if (error.response.status === 422) {
+    toast.error('¡Cliente ya Registrado!', { theme: 'colored' })
+    return
+  }
+
+  if (error.response.status === 500) {
+    toast.error('¡Problema en el Servidor!', { theme: 'colored' })
+  }
+
+  throw new Error(error)
+}
+
+// Wrapper for handling async thunks with common error handling
+const createAsyncThunkWrapper = (type, requestFn) =>
+  createAsyncThunk(`customer/${type}`, async (...args) => {
+    try {
+      const response = await requestFn(...args)
+      return response.data
+    } catch (error) {
+      handleRequestError(error)
+    }
+  })
+
+// Thunk for retrieving clients
+export const getClientes = createAsyncThunkWrapper('getClientes', async (activeToken) => {
+  return axios.get(`${API_URL}/api/v1/clientes`, {
+    headers: {
+      Authorization: atob(activeToken),
+    },
+    withCredentials: true,
+  })
 })
 
-// CREATE Clientes#create
-export const createCliente = createAsyncThunk('customer/createCliente', async ({ activeToken, newCustomer }) => {
-  try {
-    const response = await axios.post(`${API_URL}/api/v1/clientes/`, newCustomer, {
-      headers: {
-        Authorization: atob(activeToken),
-      },
-      withCredentials: true,
-    })
-
-    return response.data
-  } catch (error) {
-    if (error.response.status === 422) {
-      toast.error('¡Cliente ya Registrado!', { theme: 'colored' })
-      return
-    }
-
-    if (error.response.status === 500) {
-      toast.error('¡Problema en el Servidor!', { theme: 'colored' })
-      return
-    }
-    throw new Error(error)
-  }
-})
-// UPDATE Clientes#create
-export const updateCliente = createAsyncThunk('customer/updateCliente', async ({ activeToken, oldCustomer }) => {
-  try {
-    const response = await axios.put(`${API_URL}/api/v1/clientes/${oldCustomer.id}`, oldCustomer, {
-      headers: {
-        Authorization: atob(activeToken),
-      },
-      withCredentials: true,
-    })
-
-    return response.data
-  } catch (error) {
-    if (error.response.status === 422) {
-      toast.error('¡Cliente ya Registrado!', { theme: 'colored' })
-      return
-    }
-
-    if (error.response.status === 500) {
-      toast.error('¡Problema en el Servidor!', { theme: 'colored' })
-      return
-    }
-    throw new Error(error)
-  }
+// Thunk for creating a new client
+export const createCliente = createAsyncThunkWrapper('createCliente', async ({ activeToken, newCustomer }) => {
+  return axios.post(`${API_URL}/api/v1/clientes/`, newCustomer, {
+    headers: {
+      Authorization: atob(activeToken),
+    },
+    withCredentials: true,
+  })
 })
 
-// DELETE Clientes#destroy
-export const destroyCliente = createAsyncThunk('customer/destroyCliente', async ({ activeToken, customerID }) => {
-  try {
-    await axios.delete(`${API_URL}/api/v1/clientes/${customerID}`, {
-      headers: {
-        Authorization: atob(activeToken),
-      },
-      withCredentials: true,
-    })
-  } catch (error) {
-    if (error.response.status === 500) {
-      toast.error('¡Problema en el Servidor!', { theme: 'colored' })
-      return
-    }
-    throw new Error(error)
-  }
+// Thunk for updating an existing client
+export const updateCliente = createAsyncThunkWrapper('updateCliente', async ({ activeToken, oldCustomer }) => {
+  return axios.put(`${API_URL}/api/v1/clientes/${oldCustomer.id}`, oldCustomer, {
+    headers: {
+      Authorization: atob(activeToken),
+    },
+    withCredentials: true,
+  })
 })
 
+// Thunk for deleting a client
+export const destroyCliente = createAsyncThunkWrapper('destroyCliente', async ({ activeToken, customerID }) => {
+  return axios.delete(`${API_URL}/api/v1/clientes/${customerID}`, {
+    headers: {
+      Authorization: atob(activeToken),
+    },
+    withCredentials: true,
+  })
+})
+
+// Function to update state and stats after successful API response
+const updateStateAndStats = (state, action, successMessage) => {
+  const customers = action.payload.customers
+  state.customersOriginal = customers
+  state.customersArray = customers
+
+  /* Customer Stats */
+  state.customerStats = [
+    {
+      title: 'Clientes Registrados',
+      metric: action.payload.stats.customers_quantity,
+      color: 'bg-indigo-700',
+    },
+    {
+      title: 'Activos',
+      metric: action.payload.stats.customers_active,
+      color: 'bg-green-500',
+    },
+    {
+      title: 'Inactivos',
+      metric: action.payload.stats.customers_inactive,
+      color: 'bg-red-700',
+    },
+  ]
+
+  if (successMessage) {
+    toast.success(successMessage, { autoClose: 2000, theme: 'colored' })
+  }
+}
+
+// Redux Toolkit Slice for managing customer state
 const customerSlice = createSlice({
   name: 'customers',
   initialState,
   reducers: {
+    // Search for a customer based on input
     searchCustomer: (state, action) => {
       const searchData = action.payload.toLowerCase()
 
@@ -132,16 +140,8 @@ const customerSlice = createSlice({
 
       state.customersArray = filteredCustomers
     },
-    statusCustomer: (state, action) => {
-      const searchData = action.payload === 'true'
 
-      if (action.payload === '') {
-        state.customersArray = state.customersOriginal
-        return
-      }
-
-      state.customersArray = state.customersOriginal.filter((customer) => customer.active === searchData)
-    },
+    // Set the selected customer based on ID
     setCustomerSelected: (state, action) => {
       const content = action.payload
 
@@ -156,110 +156,29 @@ const customerSlice = createSlice({
     },
   },
   extraReducers: (builder) => {
+    // Handle API response for getClientes
     builder.addCase(getClientes.fulfilled, (state, action) => {
       state.loading = false
-      const customers = action.payload.customers
-      state.customersOriginal = customers
-      state.customersArray = customers
-
-      /* Customer Stats */
-      state.customerStats = [
-        {
-          title: 'Clientes Registrados',
-          metric: action.payload.stats.customers_quantity,
-          color: 'bg-indigo-700',
-        },
-        {
-          title: 'Activos',
-          metric: action.payload.stats.customers_active,
-          color: 'bg-green-500',
-        },
-        {
-          title: 'Inactivos',
-          metric: action.payload.stats.customers_inactive,
-          color: 'bg-red-700',
-        },
-      ]
+      updateStateAndStats(state, action)
     })
+
+    // Handle API response for createCliente
     builder.addCase(createCliente.fulfilled, (state, action) => {
-      const customers = action.payload.customers
-      state.customersOriginal = customers
-      state.customersArray = customers
-
-      /* Customer Stats */
-      state.customerStats = [
-        {
-          title: 'Clientes Registrados',
-          metric: action.payload.stats.customers_quantity,
-          color: 'bg-indigo-700',
-        },
-        {
-          title: 'Activos',
-          metric: action.payload.stats.customers_active,
-          color: 'bg-green-500',
-        },
-        {
-          title: 'Inactivos',
-          metric: action.payload.stats.customers_inactive,
-          color: 'bg-red-700',
-        },
-      ]
-
-      toast.success('¡Cliente Registrado!', { autoClose: 2000, theme: 'colored' })
+      updateStateAndStats(state, action, '¡Cliente Registrado!')
     })
+
+    // Handle API response for updateCliente
     builder.addCase(updateCliente.fulfilled, (state, action) => {
-      const customers = action.payload.customers
-      state.customersOriginal = customers
-      state.customersArray = customers
-
-      /* Customer Stats */
-      state.customerStats = [
-        {
-          title: 'Clientes Registrados',
-          metric: action.payload.stats.customers_quantity,
-          color: 'bg-indigo-700',
-        },
-        {
-          title: 'Activos',
-          metric: action.payload.stats.customers_active,
-          color: 'bg-green-500',
-        },
-        {
-          title: 'Inactivos',
-          metric: action.payload.stats.customers_inactive,
-          color: 'bg-red-700',
-        },
-      ]
-      toast.success('¡Cliente Actualizado!', { autoClose: 2000, theme: 'colored' })
+      updateStateAndStats(state, action, '¡Cliente Actualizado!')
     })
+
+    // Handle API response for destroyCliente
     builder.addCase(destroyCliente.fulfilled, (state, action) => {
-      const customers = action.payload.customers
-      state.customersOriginal = customers
-      state.customersArray = customers
-
-      /* Customer Stats */
-      state.customerStats = [
-        {
-          title: 'Clientes Registrados',
-          metric: action.payload.stats.customers_quantity,
-          color: 'bg-indigo-700',
-        },
-        {
-          title: 'Activos',
-          metric: action.payload.stats.customers_active,
-          color: 'bg-green-500',
-        },
-        {
-          title: 'Inactivos',
-          metric: action.payload.stats.customers_inactive,
-          color: 'bg-red-700',
-        },
-      ]
-
-      toast.success('¡Cliente Eliminado!', { autoClose: 2000, theme: 'colored' })
+      updateStateAndStats(state, action, '¡Cliente Eliminado!')
     })
   },
 })
 
+// Export Redux Toolkit actions and reducer
 export const customerActions = customerSlice.actions
 export default customerSlice.reducer
