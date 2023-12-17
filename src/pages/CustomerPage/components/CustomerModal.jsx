50b57328fee9e4685ba7bd1e2e192e9a31ccd52b
Rysth <johnpalacios.t@gmail.com>
2023-12-17 16:35:56 -0500

    Improve styles.

--- src/pages/CustomerPage/components/CustomerModal.jsx
+++ src/pages/CustomerPage/components/CustomerModal.jsx
@@ -1,12 +1,15 @@
 import { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { Modal } from 'flowbite-react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { customerActions } from '../../../redux/slices/CustomerSlice'
 import CustomerForm from './CustomerForm'
 
 function CustomerModal({ openModal, closeModal }) {
   const dispatch = useDispatch()
+  const { customerSelected } = useSelector((store) => store.customer)
+
+  const headerMessage = customerSelected ? 'Actualizar Cliente' : 'Nuevo Cliente'
 
   useEffect(() => {
     if (!openModal) dispatch(customerActions.setCustomerSelected(''))
@@ -14,8 +17,8 @@ function CustomerModal({ openModal, closeModal }) {
 
   return (
     <Modal show={openModal} position="center" onClose={closeModal} className="z-[9000]" size="lg" dismissible>
-      <header className="p-4 border-b">
-        <h3 className="text-xl sm:text-2xl">Nuevo Cliente</h3>
+      <header className="p-2 py-4 text-center text-white border-b rounded-t-lg bg-gradient-to-l from-blue-700 to-indigo-700">
+        <h3 className="text-xl sm:text-3xl">{headerMessage}</h3>
       </header>
       <Modal.Body>
         <CustomerForm closeModal={closeModal} />
