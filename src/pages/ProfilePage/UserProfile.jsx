f87ddd5411066ce186ceb27719b18a6814de9770
Rysth <johnpalacios.t@gmail.com>
2023-12-27 13:04:46 -0500

    Add Page Shell with KPI Sidebar

--- src/pages/ProfilePage/UserProfile.jsx
+++ src/pages/ProfilePage/UserProfile.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import SectionLayout from '../../layouts/SectionLayout'
 import { useSelector } from 'react-redux'
 import { HiUserCircle } from 'react-icons/hi2'
+import { Card, Title, Text, Grid, Col } from '@tremor/react'
 
 function UserProfile() {
   const { username } = useSelector((store) => store.authentication.activeUser)
@@ -12,6 +13,28 @@ function UserProfile() {
         <HiUserCircle className="text-8xl" />
         <h2 className="text-2xl sm:text-4xl ">{username}</h2>
       </header>
+      <Grid numItemsLg={6} className="gap-6 pb-10 mt-6">
+        {/* Main section */}
+        <Col numColSpanLg={4}>
+          <Card className="h-full">
+            <div className="h-60" />
+          </Card>
+        </Col>
+        {/* KPI sidebar */}
+        <Col numColSpanLg={2}>
+          <div className="space-y-6">
+            <Card>
+              <div className="h-24" />
+            </Card>
+            <Card>
+              <div className="h-24" />
+            </Card>
+            <Card>
+              <div className="h-24" />
+            </Card>
+          </div>
+        </Col>
+      </Grid>
     </SectionLayout>
   )
 }
