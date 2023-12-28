9092be275c9a257ea1c9ee905aaf2703629cb138
Rysth <johnpalacios.t@gmail.com>
2023-12-28 12:00:36 -0500

    Initial Commit and Improve UX in ProcessorItem.

--- src/pages/ProcessorPage/components/ProcessorItem.jsx
+++ src/pages/ProcessorPage/components/ProcessorItem.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
-import { Badge } from 'flowbite-react'
-import { Button, TableCell, TableRow } from '@tremor/react'
+import { Button, Badge } from 'flowbite-react'
+import { TableCell, TableRow } from '@tremor/react'
 import { useDispatch, useSelector } from 'react-redux'
 import { processorActions } from '../../../redux/slices/ProcessorSlice'
 
@@ -15,27 +15,27 @@ function ProcessorItem({ processor, showModal, showConfirmation }) {
 
   return (
     <TableRow key={processor.id}>
-      <TableCell className="py-2 font-bold text-gray-900 truncate whitespace-nowrap">{processor.id}</TableCell>
-      <TableCell className="py-2 truncate">{processor.cedula}</TableCell>
-      <TableCell className="py-2 truncate">{`${processor.nombres} ${processor.apellidos}`}</TableCell>
-      <TableCell className="py-2 truncate">
+      <TableCell className="py-1 font-bold text-gray-900 truncate whitespace-nowrap">{processor.id}</TableCell>
+      <TableCell className="py-1 truncate">{processor.cedula}</TableCell>
+      <TableCell className="py-1 truncate">{`${processor.nombres} ${processor.apellidos}`}</TableCell>
+      <TableCell className="py-1 truncate">
         <Badge color="indigo" className="grid place-items-center">
           {processor.user.username}
         </Badge>
       </TableCell>
-      <TableCell className="py-2 text-blue-500 truncate">
+      <TableCell className="py-1 text-blue-500 truncate">
         <a href={`tel:+593${processor.celular}`} className="text-blue-500 md:hover:text-black">
           {processor.celular}
         </a>
       </TableCell>
-      <TableCell className="flex items-center w-full gap-1 py-2">
-        <Button size="xs" onClick={() => handleProcessorSelected(processor.id)}>
+      <TableCell className="flex items-center w-full gap-1 py-1">
+        <Button size="xs" color="blue" onClick={() => handleProcessorSelected(processor.id)}>
           Editar
         </Button>
         {processor.user.id === id && (
           <Button
             size="xs"
-            color="red"
+            color="failure"
             onClick={() => {
               dispatch(processorActions.setProcessorSelected(processor.id))
               showConfirmation(true)
