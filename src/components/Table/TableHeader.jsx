3fffec0d6987eb1e3765fb4d3a5b19f4e5167b9c
Rysth <johnpalacios.t@gmail.com>
2024-01-13 21:18:01 -0500

    Improve SectionLayout and TableHEADER.

--- src/components/Table/TableHeader.jsx
+++ src/components/Table/TableHeader.jsx
@@ -7,7 +7,7 @@ import { CSVLink } from 'react-csv'
 import debounce from 'lodash/debounce'
 import { useCallback } from 'react'
 
-function TableHeader({ title, searchMethod, restartCurrentPage, showModal, originalItems, fileName }) {
+function TableHeader({ searchMethod, restartCurrentPage, showModal, originalItems, fileName }) {
   const dispatch = useDispatch()
   const [value, setValue] = useState('')
   const { usersArray } = useSelector((store) => store.users)
@@ -32,44 +32,49 @@ function TableHeader({ title, searchMethod, restartCurrentPage, showModal, origi
   }
 
   return (
-    <article className="flex flex-col items-center justify-between gap-2 p-2 sm:px-4 py-3 bg-[var(--CL-primary)] lg:flex-row rounded-t-lg">
-      <header className="flex items-center gap-1.5 text-white text-xl sm:text-2xl truncate">
-        <h3>{title}</h3>
-      </header>
-      <fieldset className="grid items-center grid-cols-2 gap-1 sm:flex">
-        <SearchSelect value={value} onValueChange={handleSelectChange} className="z-50" placeholder="Usuario">
+    <article className="p-2 sm:px-4 py-4 bg-[var(--CL-primary)] rounded-t-lg">
+      <fieldset className="flex flex-col items-center justify-between w-full gap-2 sm:flex-row">
+        <SearchSelect
+          value={value}
+          onValueChange={handleSelectChange}
+          className="z-50 max-w-md sm:max-w-sm"
+          placeholder="Usuario"
+        >
           {usersArray.map((user) => (
             <SearchSelectItem key={user.id} value={user.id} icon={IoPerson}>
               {user.username}
             </SearchSelectItem>
           ))}
         </SearchSelect>
-        <TextInput
-          id="search"
-          type="text"
-          icon={IoSearch}
-          placeholder="Buscar..."
-          onChange={handleSearchData}
-          color="purple"
-          required
-        />
-        <Button color="blue" onClick={showModal} className="flex items-center">
-          Crear
-          <IoCreateSharp className="inline-block ml-1" />
-        </Button>
-        <Button color="green">
-          <CSVLink data={originalItems} filename={`${fileName}.csv`} className="flex items-center">
-            Excel
-            <IoDownload className="ml-1" />
-          </CSVLink>
-        </Button>
+        <div className="flex flex-col items-center justify-end w-full max-w-md gap-2 sm:flex-row">
+          <TextInput
+            id="search"
+            type="text"
+            icon={IoSearch}
+            placeholder="Buscar..."
+            onChange={handleSearchData}
+            color="purple"
+            required
+          />
+          <div className="flex items-center justify-end w-full gap-1 max-w-max">
+            <Button color="blue" onClick={showModal} className="flex items-center">
+              Crear
+              <IoCreateSharp className="inline-block ml-1" />
+            </Button>
+            <Button color="green">
+              <CSVLink data={originalItems} filename={`${fileName}.csv`} className="flex items-center">
+                Excel
+                <IoDownload className="ml-1" />
+              </CSVLink>
+            </Button>
+          </div>
+        </div>
       </fieldset>
     </article>
   )
 }
 
 TableHeader.propTypes = {
-  title: PropTypes.string.isRequired,
   searchMethod: PropTypes.func.isRequired,
   restartCurrentPage: PropTypes.func.isRequired,
   showModal: PropTypes.func.isRequired,
--- src/layouts/SectionLayout.jsx
+++ src/layouts/SectionLayout.jsx
@@ -5,7 +5,7 @@ function SectionLayout({ children, title, subtitle }) {
   return (
     <section className="h-full p-4 overflow-hidden md:p-8 md:ml-60" id="customerSection">
       <article className="h-full">
-        <Title className="text-2xl font-bold text-gray-900 sm:text-4xl">{title}</Title>
+        <Title className="!text-3xl font-bold text-gray-900 sm:!text-4xl">{title}</Title>
         <Text className="text-sm sm:text-base">{subtitle}</Text>
         {children}
       </article>
